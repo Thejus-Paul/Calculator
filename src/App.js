36d5aafd0c2e64222a116e580ad507cf4a772394
Thejus Paul <thejuspaul@pm.me>
2020-08-18 13:13:10 -0700

    Add Backspace Functionality

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ function clear() {
   document.getElementById('display').innerText = '';
 }
 
+function backspace() {
+  let text = document.getElementById('display').innerText;
+  text = text.split('');
+  text.pop();
+  document.getElementById('display').innerText = text.join('');
+}
+
 function evaluate() {
   let value = eval(document.getElementById('display').innerText);
   clear();
@@ -18,7 +25,6 @@ function evaluate() {
   } else {
     document.getElementById('display').innerText = value;
   }
-  
 }
 
 function App() {
@@ -26,38 +32,37 @@ function App() {
     <div className="App">
       <span className="display" id="display"></span>
       <div className="inputs">
-      <div className="inputs__row">
-          <button className="btn" id="clear" onClick={insert.bind(this,'')}>&#10140;</button>
+      <div className="inputs__row row--1">
+          <button className="btn" id="clear" onClick={backspace.bind(this)}>&#10140;</button>
           <button className="btn operation" onClick={insert.bind(this, '/')}>&#247;</button>
           <button className="btn operation" onClick={insert.bind(this, '%')}>%</button>
           <button className="btn operation" id="backspace" onClick={clear.bind(this)}>C</button>
         </div>
-        <div className="inputs__row">
+        <div className="inputs__row row--2">
           <button className="btn operation" onClick={insert.bind(this, '*')}>&#215;</button>
           <button className="btn" onClick={insert.bind(this, '9')}>9</button>
           <button className="btn" onClick={insert.bind(this, '8')}>8</button>
           <button className="btn" onClick={insert.bind(this, '7')}>7</button>
         </div>
-        <div className="inputs__row">
+        <div className="inputs__row row--3">
           <button className="btn operation" onClick={insert.bind(this, '-')}>&#8722;</button>
           <button className="btn" onClick={insert.bind(this, '6')}>6</button>
           <button className="btn" onClick={insert.bind(this, '5')}>5</button>
           <button className="btn" onClick={insert.bind(this, '4')}>4</button>
         </div>
-        <div className="inputs__row">
+        <div className="inputs__row row--4">
           <button className="btn operation" onClick={insert.bind(this, '+')}>&#43;</button>
           <button className="btn" onClick={insert.bind(this, '3')}>3</button>
           <button className="btn" onClick={insert.bind(this, '2')}>2</button>
           <button className="btn" onClick={insert.bind(this, '1')}>1</button>
         </div>
-        <div className="inputs__row">
+        <div className="inputs__row row--5">
           <button className="btn" id="evaluate" onClick={evaluate.bind(this)}>=</button>
-          <button className="btn" onClick={insert.bind(this, '')}></button>
+          <button className="btn"></button>
           <button className="btn" onClick={insert.bind(this, '0')}>0</button>
           <button className="btn" onClick={insert.bind(this, '.')}>.</button>
         </div>
       </div>
-
     </div>
   );
 }

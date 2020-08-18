19b3dd9b18803b50cd831ba94e63cea6a71bae38
Thejus Paul <thejuspaul@pm.me>
2020-08-18 10:25:42 -0700

    UI Change
    1. Used Hex values for maths operations.
    2. Added more colors.
    3. Removed unwanted icons.
    etc.

--- src/App.js
+++ src/App.js
@@ -27,31 +27,32 @@ function App() {
       <span className="display" id="display"></span>
       <div className="inputs">
       <div className="inputs__row">
-          <button className="btn" onClick={clear.bind(this)}>C</button>
-          <button className="btn" onClick={insert.bind(this, '/')}>/</button>
-          <button className="btn" onClick={insert.bind(this, '%')}>%</button>
-          <button className="btn" onClick={insert.bind(this, '')}>()</button>
+          <button className="btn" id="clear" onClick={insert.bind(this,'')}>&#10140;</button>
+          <button className="btn operation" onClick={insert.bind(this, '/')}>&#247;</button>
+          <button className="btn operation" onClick={insert.bind(this, '%')}>%</button>
+          <button className="btn operation" id="backspace" onClick={clear.bind(this)}>C</button>
         </div>
         <div className="inputs__row">
-          <button className="btn" onClick={insert.bind(this, '*')}>*</button>
+          <button className="btn operation" onClick={insert.bind(this, '*')}>&#215;</button>
           <button className="btn" onClick={insert.bind(this, '9')}>9</button>
           <button className="btn" onClick={insert.bind(this, '8')}>8</button>
           <button className="btn" onClick={insert.bind(this, '7')}>7</button>
         </div>
         <div className="inputs__row">
-          <button className="btn" onClick={insert.bind(this, '-')}>-</button>
+          <button className="btn operation" onClick={insert.bind(this, '-')}>&#8722;</button>
           <button className="btn" onClick={insert.bind(this, '6')}>6</button>
           <button className="btn" onClick={insert.bind(this, '5')}>5</button>
           <button className="btn" onClick={insert.bind(this, '4')}>4</button>
         </div>
         <div className="inputs__row">
-          <button className="btn" onClick={insert.bind(this, '+')}>+</button>
+          <button className="btn operation" onClick={insert.bind(this, '+')}>&#43;</button>
           <button className="btn" onClick={insert.bind(this, '3')}>3</button>
           <button className="btn" onClick={insert.bind(this, '2')}>2</button>
           <button className="btn" onClick={insert.bind(this, '1')}>1</button>
         </div>
         <div className="inputs__row">
           <button className="btn" id="evaluate" onClick={evaluate.bind(this)}>=</button>
+          <button className="btn" onClick={insert.bind(this, '')}></button>
           <button className="btn" onClick={insert.bind(this, '0')}>0</button>
           <button className="btn" onClick={insert.bind(this, '.')}>.</button>
         </div>
